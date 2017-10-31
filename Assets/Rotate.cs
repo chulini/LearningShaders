8bbec01d6d223a70d8ab438a62d519c0f2358ba1
Julio Quiroz <chulini@gmail.com>
2017-10-31 17:59:24 -0300

    alpha blended texture

---
+++ Assets/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public Vector3 rotation = Vector3.up*60;
+    Transform myTransform;
+    private void Awake()
+    {
+        myTransform = GetComponent<Transform>();
+
+    }
+    void Update () {
+        myTransform.Rotate(rotation*Time.deltaTime);
+
+    }
+}
